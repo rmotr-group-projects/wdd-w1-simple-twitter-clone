8f978752f5357bc3d73797223e3935f5aad126ce
Adrian Hintermaier <adrian.hintermaier@gmail.com>
2016-08-26 20:08:24 +0200

    views skeleton

--- twitter_clone/twitter/views.py
+++ twitter_clone/twitter/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def home_page(request):
+    # need to check auth
+    return HttpResponse("you are at twitter.home_page")
+
+
+def user_profile(request):
+    return HttpResponse("you are at twitter.user_profile")
+
+
+def post_tweet(request):
+    return HttpResponse("you are at twitter.post_tweet")
+
+
+def delete_tweet(request):
+    return HttpResponse("you are at twitter.delete_tweet")
