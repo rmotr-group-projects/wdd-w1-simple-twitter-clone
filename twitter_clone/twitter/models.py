514532b03576dc61bfcfc22f7bdc725ab8f740dc
Adrian Hintermaier <adrian.hintermaier@gmail.com>
2016-08-26 21:05:08 +0200

    tweet model

--- twitter_clone/twitter/models.py
+++ twitter_clone/twitter/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Tweet(models.Model):
+    message = models.CharField(max_length=140)
+    published_date = models.DateTimeField()
+    user = models.ForeignKey(User)
