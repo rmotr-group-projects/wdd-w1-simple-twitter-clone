9ff8cbdd9676b8bcd48b59b6b9383a396c3ff1d5
Adrian Hintermaier <adrian.hintermaier@gmail.com>
2016-08-26 20:15:42 +0200

    fixed url ordering

--- twitter_clone/twitter/urls.py
+++ twitter_clone/twitter/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home_page'),
+    url(r'^login/', views.login, name='login'),
     url(r'^(?P<username>\w+)/$', views.user_profile, name='user_profile'),
     url(r'^post_tweet/(?P<tweet_id>\d+)/$', views.post_tweet, name='post_tweet'),
     url(r'^delete_tweet/(?P<tweet_id>\d+)/$', views.delete_tweet, name='delete_tweet'),
-    url(r'^login/', views.login, name='login')
 ]
