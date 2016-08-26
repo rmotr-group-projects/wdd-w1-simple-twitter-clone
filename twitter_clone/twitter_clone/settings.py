62362ef540f8a442693fd125c4825fe413639681
Adrian Hintermaier <adrian.hintermaier@gmail.com>
2016-08-26 21:40:02 +0200

    added static folder and stylesheet

--- twitter_clone/twitter_clone/settings.py
+++ twitter_clone/twitter_clone/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'twitter',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -117,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
