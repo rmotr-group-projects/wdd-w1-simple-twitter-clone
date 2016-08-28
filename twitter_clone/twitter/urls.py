fd2d96e7a61bc22d5e8f422e8f4bf6f6be1b7e5a
Christopher Pourier <corez92@gmail.com>
2016-08-28 23:58:38 +0000

    1 extra test passing, new tweet page needs to be added

--- twitter_clone/twitter/urls.py
+++ twitter_clone/twitter/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^login', auth_views.login, {'template_name': 'twitter/login.html'}),
     url(r'^tweet/(?P<tweet_id>[0-9]+)/delete', views.tweet_delete, name='tweet_delete'),
     url(r'^(?P<user_name>\w+)$', views.other_feed, name='other_feed'),
+    url(r'^tweet_created', views.tweet_created, name='tweet_created'),
 ]
--- twitter_clone/twitter/views.py
+++ twitter_clone/twitter/views.py
@@ -4,27 +4,52 @@ from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
 
 from .models import Tweet
 
 @login_required(login_url='/login?next=/')
 def index(request):
+    if request.method == 'POST' and request.POST['content']:
+        if len(request.POST['content']) > 140:
+            error = True
+            content_length = len(request.POST['content'])
+        # else:
+        #     error = None
+        #     content_length = len(request.POST['content'])
+    error = None
+    content_length = 0
     if not request.user.is_authenticated():
         return HttpResponseRedirect('/login?next=/')
     all_tweets = Tweet.objects.filter(user=request.user)
-    return render(request, "twitter/authenticated_user_feed.html", {"all_tweets": all_tweets})
+    context = {
+        "all_tweets": all_tweets,
+        "error": error,
+        "content_length": content_length
+    }
+    return render(request, "twitter/authenticated_user_feed.html", context)
 
 def login_page(request):
     return render(request, "twitter/login.html")
-    
+
+@login_required(login_url='/login?next=/')    
 def tweet_delete(request, tweet_id):
     #/tweet/{{tweet.id}}/delete?next=/
+    # if request.user.is_authenticated():
     try:
         del_tweet = Tweet.objects.get(pk=tweet_id, user=request.user)
     except ObjectDoesNotExist:
         raise PermissionDenied
     del_tweet.delete()
+    return HttpResponseRedirect('/')
+    #print(settings.LOGIN_URL)
+    # return HttpResponseRedirect('%s?next=%s' % ('/login', request.path))#('/login?next=/')
+
+@login_required(login_url='/login?next=/')       
+def tweet_created(request):
+    new_tweet = Tweet.objects.create(user=request.user.username, content=request.POST['content'])
+    new_tweet.save()
     return HttpResponseRedirect('/')
 
 def other_feed(request, user_name):
